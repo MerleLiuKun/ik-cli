fa336c86e30aef753075c5156c18c8cf65b2a81e
IkarosKun <merle.liukun@gmail.com>
2019-07-28 21:55:57 +0800

    feat(ip info): :star2: improve ip info to show

--- ik_cli/cli.py
+++ ik_cli/cli.py
@@ -1,7 +1,28 @@
+import sys
 from pathlib import Path
 
 import click
 
+
+class Context:
+    def __init__(self):
+        self.verbose = False
+        self.debug = False
+
+    def log(self, msg, *args, color=None):
+        if args:
+            msg %= args
+        color = color
+        if self.debug:
+            color = 'red'
+        click.echo(msg, file=sys.stderr, color=color)
+
+    def v_log(self, msg, *args):
+        if self.verbose:
+            self.log(msg, *args, color='yellow')
+
+
+pass_context = click.make_pass_decorator(Context, ensure=True)
 cmd_folder = Path(__file__).cwd() / 'commands'
 
 
@@ -23,5 +44,7 @@ class ComplexCLI(click.MultiCommand):
 
 
 @click.command(cls=ComplexCLI)
-def cli():
-    pass
+@click.option('-v', '--verbose', is_flag=True, help='Enable verbose mode.')
+@pass_context
+def cli(ctx, verbose):
+    ctx.verbose = verbose
